e7d4de5415c06ceeaaf8d939d5a6c1d4ab0b1221
Duy Khanh <162108959+this-is-duykhanh@users.noreply.github.com>
2025-09-02 10:59:35 +0700

    implement demotration livestream application intergation with livekit server

---
+++ 20-livestream-livekit-application/Livekit-springboot/src/main/java/com/livekit/Livekit_springboot/LivekitSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.livekit.Livekit_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivekitSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivekitSpringbootApplication.class, args);
+	}
+
+}
